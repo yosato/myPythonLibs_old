e4d8a0085787c320fae7edd11b879c701a0721ac
Yo Sato <yosato16@gmail.com>
2015-09-19 11:46:59 +0100

    dist stuff

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = "pythonlib mine",
+    packages = ['pythonlib'],
+    version = "1.0.2",
+    description = 'my own stuff for python',
+    author = "Yo Sato",
+    author_email = "yosato16@gmail.com",
+    download_url = "https://github.com/yosato/myProgs/pythonlib/pythonlib.tar.gz",
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        ],
+    long_description = """\
+just stuff
+
+"""
+)
