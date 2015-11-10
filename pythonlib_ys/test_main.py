4a28799dbcbc53ffed5a278eef681aae100e7b94
Yo Sato <yo.sato@myscript.com>
2015-11-10 07:25:45 +0100

    adding test for my module

---
+++ pythonlib_ys/test_main.py
@@ -0,0 +1,31 @@
+import unittest,os,imp
+import main
+imp.reload(main)
+from pdb import set_trace
+
+class TestFileRelated(unittest.TestCase):
+    def setUp(self):
+
+        self.testDir=os.path.join(os.getenv('HOME'),'links/corpora_mine_test')
+        self.testFP=os.path.join(self.testDir,'sample.txt')
+        self.testLines1='aaa\niii\nuuu\neee\nooo\n'
+        self.testLines2='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\niiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii\nuuu\neeeeeeeeeeee\nooooooooo\n'
+    def test_readline_reverse(self):
+        set_trace()
+        for TestLines in (self.testLines1,self.testLines2):
+            open(self.testFP,'wt').write(self.testLines)
+            FSr=open(self.testFP,'rt')
+            FSr.readline();FSr.readline();FSr.readline()
+            LstLine=main.readline_reverse(FSr)
+            FSr.close()
+            self.assertEqual(LstLine,'uuu')
+    def test_get_nths_lines(self):
+        set_trace()
+        open(self.testFP,'wt').write(self.testLines1)
+        AllegedLines=main.get_nths_lines(self.testFP,[3,5,2])
+        self.assertEqual(AllegedLines,['uuu','ooo','iii'])
+           
+            
+
+if __name__=='__main__':
+    unittest.main()
